af3bf8704833bc017e7cfffb610dbd8d1bd2bc81
malasiot <malasiot@gmail.com>
2026-06-11 12:07:58 +0300

    dependency

--- CMake/dependencies.cmake
+++ CMake/dependencies.cmake
@@ -3,10 +3,10 @@ if(APPLE)
         "/opt/homebrew/opt/sqlite"     # Apple Silicon (M1/M2/M3/M4)
         "/usr/local/opt/sqlite"         # Intel Macs
     )
+    SET(PostgreSQL_ADDITIONAL_VERSIONS 17 18)
 endif()
 
 find_package(SQLite3 REQUIRED)
-message(${SQLite3_INCLUDE_DIR})
-find_package(PostgreSQL QUIET)
+find_package(PostgreSQL REQUIRED)
 find_package(PkgConfig REQUIRED)
 pkg_check_modules(MARIADB REQUIRED IMPORTED_TARGET libmariadb)
